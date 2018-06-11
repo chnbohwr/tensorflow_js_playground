a0dc6a3a8dc3274f8d34403995f139837dd62a9f
Hyman_Chen <hyman_chen@wistron.com>
2018-06-11 16:01:51 +0800

    add hidden layer to parameter

--- src/app.js
+++ src/app.js
@@ -9,80 +9,145 @@ window.tf = tf;
 
 export default class App extends Component {
   state = {
-    layers: [{ units: 1 }, { units: 100 }, { units: 100 }, { units: 100 }, { units: 1 }],
     inputShape: [1],
     outputShape: [1],
-    batchSize: 1000,
-    epochs: 10,
-    model: null,
+    batchSize: 200,
+    activation: 'elu',
+    hiddenLayers: 3,
+    hiddenUnits: 3,
+    epochs: 200,
+    isLoading: false,
+    hasModel: false,
   }
   onChangeBatchSize = (e) => {
     this.setState({
       batchSize: parseInt(e.target.value),
-      model: null
     });
   }
   onChangeEpochs = (e) => {
     this.setState({
       epochs: parseInt(e.target.value),
-      model: null
     });
   }
+  onChangeActivation = (e) => {
+    this.setState({
+      activation: e.target.value
+    });
+  }
+  onChangeHiddenLayers = (e) => {
+    this.setState({
+      hiddenLayers: parseInt(e.target.value)
+    });
+  }
+  onChangeHiddenUnits = (e) => {
+    this.setState({
+      hiddenUnits: parseInt(e.target.value)
+    });
+  }
+  onClickReset = () => {
+    this.setState({ hasModel: false, isLoading: false, data: null })
+  }
   onClickCreateModel = () => {
+    const { activation, hiddenLayers, hiddenUnits } = this.state
     const model = tf.sequential();
     const xsArr = Array
       .from(Array(this.state.batchSize))
       .map(() => random.float(-5, 5))
       .sort((a, b) => (a - b));
     const ysArr = xsArr.map(d => Math.sin(d));
-    this.state.layers.forEach((d, i) => {
-      const option = { units: d.units };
-      if (i === 0) {
-        option.inputShape = this.state.inputShape;
-      }
-      if (i === this.state.layers.length - 1) {
-        option.outputShape = this.state.outputShape;
-      }
-      console.log(option);
-      const layer = tf.layers.dense(option);
-      model.add(layer);
-    });
-    model.compile({ loss: tf.losses.meanSquaredError, optimizer: 'sgd' });
+    // this.state.layers.forEach((d, i) => {
+    //   const option = { units: d.units, useBias: true, activation };
+    //   if (i === 0) {
+    //     option.inputShape = this.state.inputShape;
+    //   }
+    //   if (i === this.state.layers.length - 1) {
+    //     option.outputShape = this.state.outputShape;
+    //   }
+    //   const layer = tf.layers.dense(option);
+    //   model.add(layer);
+    // });
+    model.add(tf.layers.dense({ units: 1, useBias: true, inputShape: [1] }));
+    for (let i = 0; i < hiddenLayers; i++) {
+      model.add(tf.layers.dense({ units: hiddenUnits, useBias: true, activation }));
+    }
+    model.add(tf.layers.dense({ units: 1, useBias: true }));
+    model.compile({ loss: 'meanSquaredError', optimizer: 'adam' });
     this.model = model;
     this.xsArr = xsArr;
     this.ysArr = ysArr;
+    window.model = model;
+    console.log('create model success');
+    this.setState({ hasModel: true });
+    this.onClickTraining();
   }
   onClickTraining = () => {
     const { epochs, } = this.state;
+    this.setState({ isLoading: true });
     const xs = tf.tensor2d(this.xsArr, [this.state.batchSize, 1]);
     const ys = tf.tensor2d(this.ysArr, [this.state.batchSize, 1]);
-    tf.tidy(() => {
-      this.model.fit(xs, ys).then(() => {
-        const predictArr = this.model.predictOnBatch(xs).dataSync();
-        const data = Array.from(predictArr)
-          .map((predict, i) => ({
-            name: this.xsArr[i],
-            answer: this.ysArr[i],
-            predict: predict
-          }));
-        this.setState({ data });
-      });
+    console.log('hi1');
+
+    console.log('hi2');
+    this.model.fit(xs, ys, { epochs }).then((f) => {
+      console.log(f);
+      const predictArr = this.model.predictOnBatch(xs).dataSync();
+      console.log('hi4')
+      const data = Array.from(predictArr)
+        .map((predict, i) => ({
+          name: this.xsArr[i],
+          answer: this.ysArr[i],
+          predict: predict
+        }));
+      this.setState({ data, isLoading: false });
     });
+    console.log('hi3');
   }
   render() {
-    const { batchSize, epochs, data } = this.state;
+    const {
+      batchSize, epochs, data,
+      activation, hiddenLayers, hiddenUnits,
+      hasModel, isLoading,
+    } = this.state;
     return (
-      <div>
+      <div style={{ padding: 20 }}>
+        <FormGroup>
+          <Label>隱藏層數目</Label>
+          <Input disabled={isLoading} type="number" name="hiddenLayers" value={hiddenLayers} onChange={this.onChangeHiddenLayers} />
+        </FormGroup>
+        <FormGroup>
+          <Label>隱藏層神經元數目</Label>
+          <Input disabled={isLoading} type="number" name="hiddenUnits" value={hiddenUnits} onChange={this.onChangeHiddenUnits} />
+        </FormGroup>
         <FormGroup>
           <Label>訓練樣本數</Label>
-          <Input type="number" name="batchsize" value={batchSize} onChange={this.onChangeBatchSize} />
+          <Input disabled={isLoading} type="number" name="batchsize" value={batchSize} onChange={this.onChangeBatchSize} />
         </FormGroup>
         <FormGroup>
           <Label>訓練次數</Label>
-          <Input type="number" name="epoch" value={epochs} onChange={this.onChangeEpochs} />
+          <Input disabled={isLoading} type="number" name="epoch" value={epochs} onChange={this.onChangeEpochs} />
         </FormGroup>
-        <Button color="success" onClick={this.onClickCreateModel}>建立模型!</Button>
-        <Button color="success" onClick={this.onClickTraining}>開始訓練!</Button>
+        <FormGroup>
+          <Label>激勵函數</Label>
+          <Input disabled={isLoading} value={activation} type="select" name="activation" onChange={this.onChangeActivation}>
+            <option>elu</option>
+            <option>relu</option>
+            <option>relu6</option>
+            <option>sigmoid</option>
+            <option>softmax</option>
+          </Input>
+        </FormGroup>
+        {
+          hasModel ? (
+            <div>
+              <Button disabled={isLoading} style={{ marginRight: 20 }} color="primary" onClick={this.onClickTraining}>再訓練</Button>
+              <Button disabled={isLoading} color="primary" onClick={this.onClickReset}>重置</Button>
+            </div>
+          ) : (
+              <Button disabled={isLoading} color="success" onClick={this.onClickCreateModel}>建立模型</Button>
+            )
+        }
+
+
         {
           data &&
           (
@@ -102,25 +167,3 @@ export default class App extends Component {
     )
   }
 }
-
-
-// const App = () => {
-//   const model = tf.sequential();
-//   model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
-
-//   // Prepare the model for training: Specify the loss and the optimizer.
-//   model.compile({ loss: tf.losses.meanSquaredError, optimizer: 'sgd' });
-
-//   const length = 10;
-//   // Generate some synthetic data for training.
-//   const xarr = Array.from(Array(length)).map((d, i) => i);
-//   const yarr = Array.from(Array(length)).map((d, i) => i * 10);
-
-//   const xs = tf.tensor2d(xarr, [length, 1]);
-//   const ys = tf.tensor2d(yarr, [length, 1]);
-
-//   model.fit(xs, ys, { epochs: 250 }).then(() => {
-//     model.predict(tf.tensor2d([2], [1, 1])).print();
-//   });
-
-// }
